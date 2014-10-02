708a9abf89d59197c8aaf42f5d6f97c13493c869
Kenny Domingo <kennydomingo@kennys-mbp.home>
2014-10-02 17:35:06 +0100

    Initial Commit

---
+++ Fireball Soccer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Fireball Soccer
+//
+//  Created by Kenny Domingo on 02/10/2014.
+//  Copyright (c) 2014 Kenny Domingo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Fireball Soccer/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Fireball Soccer
+//
+
+//  Copyright (c) 2014 Kenny Domingo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface ViewController : UIViewController
+
+@end
